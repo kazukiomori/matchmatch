6b7bb4913b3a6d5c5e981f8ae4b1809673554d6c
kazukiomori <kazuki1110170501@gmail.com>
2024-05-16 23:12:49 +0900

    Initial Commit

---
+++ matchmacth/matchmacthApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchmacthApp.swift
+//  matchmacth
+//
+//  Created by Kazuki Omori on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct matchmacthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
